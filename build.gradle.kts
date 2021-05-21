c3dc770ef4e381ecfb0a30d32f180787bb94778f
Doctor_Yin <1921841421@qq.com>
2021-05-21 18:10:14 +0800

    :new: 添加依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,13 @@ group = "top.limbang"
 version = "1.0.0"
 
 repositories {
+
+    maven("http://web.blackyin.top:8015/repository/maven-public/")
     maven("https://maven.aliyun.com/repository/public")
     mavenCentral()
 }
 
-dependencies{
+dependencies {
     testImplementation("junit:junit:4.12")
+    implementation("top.limbang:doctor:1.0.0")
 }
