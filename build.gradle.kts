211990cf38741f3832bad77179babb621de0077f
limbang <495071565@qq.com>
2021-08-27 12:35:58 +0800

    :arrow_up: 升级依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,14 @@ plugins {
 }
 
 group = "top.limbang"
-version = "1.1.4"
+version = "1.1.5"
 
 repositories {
-    maven("https://maven.blackyin.xyz:8015/repository/maven-public/")
-    maven("https://maven.aliyun.com/repository/public")
+    maven("https://maven.fanua.top:8015/repository/maven-public/")
     mavenCentral()
 }
 
 dependencies {
-    implementation("top.limbang.doctor:doctor-client:1.2.7")
+    implementation("top.fanua.doctor:doctor-client:1.3.4-dev-5")
     testImplementation("org.slf4j:slf4j-simple:1.7.30")
 }
